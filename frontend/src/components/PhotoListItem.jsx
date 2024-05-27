85c4a8bacef74df2bfacdeec408a1a07639b5afa
NofarZiv <nofarziv13@gmail.com>
2024-05-27 14:37:39 -0230

    the PhotoListItem component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,20 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
-  /* Insert React */
+
+const PhotoListItem = (props) => {
+
+  const { sampleDataForPhotoListItem } = props
+  
+  return (
+    <article className="item">
+    <img src={sampleDataForPhotoListItem.imageSource} />
+    <img src={sampleDataForPhotoListItem.profile} />
+    <p> {sampleDataForPhotoListItem.username}</p>
+    <p> {sampleDataForPhotoListItem.location.city}, {sampleDataForPhotoListItem.location.country} </p>
+    </article>
+  );
 };
 
 export default PhotoListItem;
