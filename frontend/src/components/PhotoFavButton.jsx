2794067348a5737ff0eb84283ec14504a42791ba
NofarZiv <nofarziv13@gmail.com>
2024-05-27 18:29:32 -0230

    add state to show when the icon is liked and add the FavIcon component.

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,13 +4,21 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
-  return (
-    <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+
+  const [like, setLike] = useState(false)
+  const showLike = () => {
+   setLike(!like);
+  }
+
+    return (
+      <div onClick={showLike} className="photo-list__fav-icon">
+        <div className="photo-list__fav-icon-svg">
+          <FavIcon like={like}/>
+        </div>
       </div>
-    </div>
-  );
+      
+    );
 }
 
 export default PhotoFavButton;
+
