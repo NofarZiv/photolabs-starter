2b47220df18a17db4f2a3e568cc4763acc335059
NofarZiv <nofarziv13@gmail.com>
2024-05-28 14:27:17 -0230

    import data from topics.js.

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,26 +1,10 @@
 import React from "react";
 import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
+import topics from "mocks/topics";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
 
-const topics = sampleDataForTopicList.map(topic => {
+const topicsArr = topics.map(topic => {
   return (
    <TopicListItem 
     key={topic.id}
@@ -30,10 +14,11 @@ const topics = sampleDataForTopicList.map(topic => {
   );
 });
 
+
 const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
-      {topics}
+      {topicsArr}
     </div>
   );
 };
