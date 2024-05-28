8fbeb28620d38aa7644810f6fcccfad93ad46f22
NofarZiv <nofarziv13@gmail.com>
2024-05-28 14:56:08 -0230

    add props to get data from the parent component.

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
-import topics from "mocks/topics";
 
 
-const topicsArr = topics.map(topic => {
-  return (
-   <TopicListItem 
-    key={topic.id}
-    slug={topic.slug}
-    title={topic.title}
-  />
-  );
-});
+
+const TopicList = (props) => {
+
+  const { topics } = props
+  
+  const topicsArr = topics.map(topic => {
+    return (
+     <TopicListItem 
+      key={topic.id}
+      slug={topic.slug}
+      title={topic.title}
+    />
+    );
+  });
 
 
-const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
       {topicsArr}
