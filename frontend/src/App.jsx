324025ca737401779d67209cc5674795883937a7
NofarZiv <nofarziv13@gmail.com>
2024-05-28 14:26:09 -0230

    clean the code.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import PhotoList from 'components/PhotoList';
-import TopicList from 'components/TopicList';
-import TopNavigation from 'components/TopNavigationBar';
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
 
 
-// Note: Rendering a single component to build components in isolation
+
 const App = () => {
-
-  // const arr = [];
-  // for (let i = 0; i < 3; i++) {
-  //   arr.push(<PhotoListItem key={i} sampleDataForPhotoListItem={sampleDataForPhotoListItem}/>);
-  // }
-
 
   return (
     <div className="App">
-      {/* {arr} */}
-      {/* <TopNavigation />
-      <PhotoList /> */}
       <HomeRoute />
     </div>
   );
