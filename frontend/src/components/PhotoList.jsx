5872dfbf6b27c95e663b217dedeaa315b6fa2fab
NofarZiv <nofarziv13@gmail.com>
2024-05-28 14:58:10 -0230

    add props to get data from the parent component.

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import PhotoListItem from "./PhotoListItem";
 import "../styles/PhotoList.scss";
-import photos from "mocks/photos";
+// import photos from "mocks/photos";
 
 
-const items = photos.map(item => {
-  return (
-    <PhotoListItem
-      key={item.id}
-      imageSource={item.urls.regular}
-      profile={item.user.profile}
-      name={item.user.name}
-      city={item.location.city}
-      country={item.location.country}
-    />
-  );
-});
+const PhotoList = (props) => {
 
-const PhotoList = () => {
+  const { photos } = props
+
+  const items = photos.map(item => {
+    return (
+      <PhotoListItem
+        key={item.id}
+        imageSource={item.urls.regular}
+        profile={item.user.profile}
+        name={item.user.name}
+        city={item.location.city}
+        country={item.location.country}
+      />
+    );
+  });
+
+
   return (
     <ul className="photo-list">
       {items}
