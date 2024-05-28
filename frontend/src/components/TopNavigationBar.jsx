6da230b343e03be070d1f343685ab849dc718b48
NofarZiv <nofarziv13@gmail.com>
2024-05-28 13:33:48 -0230

    changed the FavIcon component to the FavBadge component.

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import TopicList from './TopicList';
-import FavIcon from './FavIcon';
+import FavBadge from './FavBadge';
 import '../styles/TopNavigationBar.scss'
 
-const like = true;
+
 
 const TopNavigation = () => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList />
-      <FavIcon like={like}/>
+      <FavBadge />
     </div>
   )
 }
