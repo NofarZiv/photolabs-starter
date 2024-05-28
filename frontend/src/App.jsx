1fc0e400f1d019bb3be7a7dbe1dc70ecc08629ab
NofarZiv <nofarziv13@gmail.com>
2024-05-28 14:49:03 -0230

    import mock data from photos.js and topics.js.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
+import topics from "mocks/topics";
+import photos from "mocks/photos";
 
 
 
@@ -8,7 +10,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <HomeRoute />
+      <HomeRoute topics={topics} photos={photos}/>
     </div>
   );
 };
