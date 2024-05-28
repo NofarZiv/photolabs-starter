13d57f8a7263e8c5923702112a0ce797b7a9c846
NofarZiv <nofarziv13@gmail.com>
2024-05-28 19:08:39 -0230

    create state to handle likes and pass the function to the child component.

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {useState} from 'react';
 import TopNavigation from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
 import '../styles/HomeRoute.scss';
@@ -8,11 +8,17 @@ const HomeRoute = (props) => {
 
   const { photos, topics } = props
 
+  const [heart, setHeart] = useState(0)
+  const handlingHeart = () => {
+    setHeart(heart + 1)
+    console.log(heart)
+  }
+
 
   return (
     <div className="home-route">
        <TopNavigation topics={topics}/>
-       <PhotoList photos={photos}/>
+       <PhotoList photos={photos} handlingHeart={handlingHeart}/>
     </div>
   );
 };
