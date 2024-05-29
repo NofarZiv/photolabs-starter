558d6b39eda4fc253cfe27a201a150966ec083dd
NofarZiv <nofarziv13@gmail.com>
2024-05-29 13:15:44 -0230

    add a state to track modal, pass it to the child component and add conditional rendering for the modal component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import {useState} from 'react';
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
 import topics from "mocks/topics";
 import photos from "mocks/photos";
-
-
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
 const App = () => {
 
+  const [modal, setModal] = useState(false)
+  const showModal = () => {
+    setModal(!modal)
+  }
+
   return (
     <div className="App">
-      <HomeRoute topics={topics} photos={photos}/>
+      <HomeRoute topics={topics} photos={photos} showModal={showModal}/>
+      {modal === true && <PhotoDetailsModal />}
     </div>
   );
 };
