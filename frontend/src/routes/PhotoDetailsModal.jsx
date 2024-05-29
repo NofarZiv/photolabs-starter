a1d1b105489a8b04b02dc98b85d98a5d7588a068
NofarZiv <nofarziv13@gmail.com>
2024-05-29 13:41:30 -0230

    add an onClick event to close the modal.

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,10 +3,13 @@ import React from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = (props) => {
+
+  const {closeModal} = props 
+
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
+      <button onClick={closeModal} className="photo-details-modal__close-button">
         <img src={closeSymbol} alt="close symbol" />
       </button>
     </div>
