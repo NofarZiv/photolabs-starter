101d9adec652c5c61732db51f3a099d5eb4308c4
NofarZiv <nofarziv13@gmail.com>
2024-05-31 13:44:57 -0230

    change name to username.

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -22,7 +22,7 @@ const PhotoListItem = (props) => {
       <img onClick={handleClick} className="photo-list__image" src={urls.regular} />
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={user.profile} />
-        <p className="photo-list__user-info"> {user.name}
+        <p className="photo-list__user-info"> {user.username}
         </p>
         <p className="photo-list__user-location"> {location.city}, {location.country} </p>
       </div>
