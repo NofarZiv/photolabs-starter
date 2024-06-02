05abfc14c5dae4ca00fed5a4c26ef7a2994c6d5e
NofarZiv <nofarziv13@gmail.com>
2024-06-02 17:16:23 -0230

    modify the photos and topics to get the data from the state object.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
-import topics from "mocks/topics";
-import photos from "mocks/photos";
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 import useApplicationData from 'hooks/useApplicationData';
 
 
 const App = () => {
+
 
   const {
     state,
@@ -17,10 +16,9 @@ const App = () => {
   } = useApplicationData();
 
 
-
   return (
     <div className="App">
-      <HomeRoute topics={topics} photos={photos} showModal={showModal} favorites={state.favorites} handlingFavorites={handlingFavorites}/>
+      <HomeRoute topics={state.topicData} photos={state.photoData} showModal={showModal} favorites={state.favorites} handlingFavorites={handlingFavorites}/>
       {state.modal !== null &&  <PhotoDetailsModal closeModal={closeModal} selectedPhoto={state.modal} favorites={state.favorites} handlingFavorites={handlingFavorites}/>}
     </div>
   );
