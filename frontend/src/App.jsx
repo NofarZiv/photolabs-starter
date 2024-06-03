53d4b4a7b1389a773e76f2c17554d36eddd8ab74
NofarZiv <nofarziv13@gmail.com>
2024-06-03 13:12:35 -0230

    pass topicPhotos and handlePhotos to child component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,12 +13,14 @@ const App = () => {
     showModal,
     closeModal,
     handlingFavorites,
+    handlePhotos,
   } = useApplicationData();
 
+  
 
   return (
     <div className="App">
-      <HomeRoute topics={state.topicData} photos={state.photoData} showModal={showModal} favorites={state.favorites} handlingFavorites={handlingFavorites}/>
+      <HomeRoute topics={state.topicData} photos={state.photoData} showModal={showModal} favorites={state.favorites} handlingFavorites={handlingFavorites} handlePhotos={handlePhotos} topicPhotos={state.topicPhotos}/>
       {state.modal !== null &&  <PhotoDetailsModal closeModal={closeModal} selectedPhoto={state.modal} favorites={state.favorites} handlingFavorites={handlingFavorites}/>}
     </div>
   );
