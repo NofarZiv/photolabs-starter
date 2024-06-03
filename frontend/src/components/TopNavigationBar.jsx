5ef39c47b3d4039f4a14e2cdd5f3cb5cdefc76cf
NofarZiv <nofarziv13@gmail.com>
2024-06-03 13:15:19 -0230

    pass handlePhotos function to child component.

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -7,13 +7,13 @@ import '../styles/TopNavigationBar.scss'
 
 const TopNavigation = (props) => {
 
-  const { topics, favorites } = props
+  const { topics, favorites, handlePhotos} = props
 
 
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList topics={topics}/>
+      <TopicList topics={topics} handlePhotos={handlePhotos}/>
       <FavBadge isFavPhotoExist={favorites.length > 0 ? true : false}/>
     </div>
   )
