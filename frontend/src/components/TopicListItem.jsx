8be99b419ad3a8d31b7efcd13ef568473645f483
NofarZiv <nofarziv13@gmail.com>
2024-06-03 13:17:07 -0230

    add onClick event and call the handlePhotos function.

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-
 import "../styles/TopicListItem.scss";
 
 
 const TopicListItem = (props) => {
 
-  const { slug, title } = props
+  const { id, title, handlePhotos } = props
+
+  const handleClickTopic = () => {
+    handlePhotos(id)
+  }
+
 
   return (
-    <div className="topic-list__item">
+    <div onClick={handleClickTopic} className="topic-list__item">
       <span> {title}</span>
     </div>
   );
